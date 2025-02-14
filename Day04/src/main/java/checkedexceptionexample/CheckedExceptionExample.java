62757fb5e9a140193539b0aa9a6c148fbf15b46a
aditya12696 <aditysharma127@gmail.com>
2025-02-14 23:34:54 +0530

    Aditya Added Checked Exception handling program

---
+++ Day04/src/main/java/checkedexceptionexample/CheckedExceptionExample.java
@@ -0,0 +1,18 @@
+package checkedexceptionexample;
+
+import java.io.*;
+
+public class CheckedExceptionExample {
+    public static void main(String[] args) {
+        String fileName = "data.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
